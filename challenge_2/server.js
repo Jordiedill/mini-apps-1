2bdc14352088dda2c8ffa12eb673e6fd5a3f9603
Jordan <jordan.dillard13@gmail.com>
2019-06-25 16:48:26 -0500

    post request functioning

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 
-app.use(express.static('client'));
+app.use('/', express.static(path.join(__dirname, 'client')));
+//app.use(urlencoded({extended: true}));
+
+app.post('/submitform', ((req,res) => {
+    res.send('YAY POSTING')
+    console.log('YAY POSTING!');
+}))
+
+app.get('/', ((req, res) => {
+  res.send('Yay')
+}))
 
 console.log('Yay!');
 
